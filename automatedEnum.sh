16f5245bdbd84c52f916893090e1d239429ad5a2
MrW0l05zyn <marcelo@wolcas.cl>
2021-10-07 03:40:36 -0400

    automatedEnum.sh

--- automatedEnum.sh
+++ automatedEnum.sh
@@ -5,7 +5,7 @@ target=''
 mode='basic'
 modes=(basic vuln full)
 service=''
-services=(FTP HTTP HTTPS SMB SMTP SNMP SSH)
+services=(FTP HTTP HTTPS RDP SMB SMTP SNMP SSH)
 TCPPortsTarget=''
 UDPPortsTarget=''
 parameterCounter=0
@@ -14,6 +14,8 @@ vulnDirectory='vulns'
 workingDirectory='.working'
 topUDPPorts='53,67,68,69,111,123,135,137,138,139,161,162,445,500,514,520,631,998,1434,1701,1900,4500,5353,49152,49154'
 version='0.1'
+indentation1='   '
+indentation2='      '
 
 # colores
 RED='\033[0;31m'
@@ -23,6 +25,9 @@ BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[0m'
+
+# bold
+BGREEN='\033[1;32m'
 
 # función de banner
 function banner(){ 
@@ -43,7 +48,7 @@ function usage() {
     echo -e "\nOptions:"
     echo -e "\t-t <TARGET>\tTarget/Host IP address"
     echo -e "\t-m <MODE>\tMode: basic|vuln|full (default: basic)"
-    echo -e "\t-s <SERVICE>\tService name: FTP|HTTP|HTTPS|SMB|SMTP|SNMP|SSH"
+    echo -e "\t-s <SERVICE>\tService name: FTP|HTTP|HTTPS|RDP|SMB|SMTP|SNMP|SSH"
     echo -e "\t-h \t\tShows instructions on how to use the tool"
 
     echo -e "\nExamples:"
@@ -60,7 +65,7 @@ function targetParameterValidation(){
     regexIP='^(0*(1?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))\.){3}0*(1?[0-9]{1,2}|2([‌​0-4][0-9]|5[0-5]))$'
     regexHostname='^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$'
 
-    # IP address y hostname 
+    # Validación de dirección IP y hostname
     if [[ ! $1 =~ $regexIP ]] && [[ ! $1 =~ $regexHostname ]]; then
         echo -e "\n${YELLOW}Invalid target \"(-t)\" argument.${NC}"
         usage
@@ -164,22 +169,38 @@ function nmapPortsExtract(){
 
 # función de spinner para ejecución de procesos
 function spinner(){
+    local indentation='';
     local info="$1"
     local pid=$!
     local delay=0.25
     local spinstr='|/-\'
+
+    case $2 in        
+        1) indentation=$indentation1;;
+        2) indentation=$indentation2;;
+    esac
+
     while kill -0 $pid 2> /dev/null; do
         local temp=${spinstr#?}
-        printf "${PURPLE}[%c] $info${NC}" "$spinstr"
+        printf "${PURPLE}$indentation[%c] $info${NC}" "$spinstr"
         local spinstr=$temp${spinstr%"$temp"}
         sleep $delay
         local reset="\b\b\b\b\b\b"
-        for ((i=1; i<=$(echo $info | wc -c); i++)); do
+        for ((i=1; i<=$(echo -e "$indentation$info" | wc -c); i++)); do
             reset+="\b"
         done
         printf $reset
     done
-    printf "${GREEN}[\u2713] $info${NC}\n"
+
+    case $2 in        
+        2) printf "$indentation[\u2713] $info\n";;
+        *) printf "${GREEN}$indentation[\u2713] $info${NC}\n";;        
+    esac    
+}
+
+# función de título de etapa en proceso
+function stageProcessTitle(){
+    echo -e "${BGREEN}[${1^^}]${NC}"
 }
 
 # función de escaneo con Nmap
@@ -189,39 +210,42 @@ function nmapScan(){
     # creación de directorio Nmap
     directoryCreation $directory    
 
+    # título de etapa en proceso
+    echo ''; stageProcessTitle "Port Scan"
+
     # TCP all port scan
     sudo nmap -sS -p- --open -n --min-rate 5000 -Pn $target -oN $mainDirectory/$directory/all-tcp-ports.txt -oG $mainDirectory/$workingDirectory/tcp-ports &> /dev/null &
-    echo ''; spinner "[Nmap - TCP all port scan]"    
+    echo ''; spinner "[Nmap - TCP all port scan]" 1
 
     # extracción de puertos TCP
     nmapPortsExtract 'TCP'
 
     # muestra puertos TCP encontrados
     if [ -n "$TCPPortsTarget" ]; then
-        echo ''; cat $mainDirectory/$directory/all-tcp-ports.txt | grep -v '#' | grep 'PORT\|[0-9]/tcp'
+        echo ''; cat $mainDirectory/$directory/all-tcp-ports.txt | grep -v '#' | grep 'PORT\|[0-9]/tcp' | sed "s/.*/$indentation2&/g"
     else
-        echo ''; echo -e "${YELLOW}TCP ports not found.${NC}"
+        echo ''; echo -e "\t${YELLOW}TCP ports not found.${NC}"
     fi
 
     # UDP main port scan    
     sudo nmap -sU -p $topUDPPorts --open -n -Pn $target -oN $mainDirectory/$directory/main-udp-ports.txt -oG $mainDirectory/$workingDirectory/udp-ports &> /dev/null &
-    echo ''; spinner "[Nmap - UDP main port scan]"
+    echo ''; spinner "[Nmap - UDP main port scan]" 1
 
     # extracción de puertos UDP
     nmapPortsExtract 'UDP'
 
     # muestra puertos UDP encontrados
     if [ -n "$UDPPortsTarget" ]; then
-        echo ''; cat $mainDirectory/$directory/main-udp-ports.txt | grep -v '#' | grep 'PORT\|[0-9]/udp'
+        echo ''; cat $mainDirectory/$directory/main-udp-ports.txt | grep -v '#' | grep 'PORT\|[0-9]/udp' | sed "s/.*/$indentation2&/g"
     else
-        echo ''; echo -e "${YELLOW}UDP ports not found.${NC}"
+        echo ''; echo -e "\t${YELLOW}UDP ports not found.${NC}"
     fi   
 
     # identificación de servicios
     if [ -n "$TCPPortsTarget" ]; then
         nmap -sC -sV -p $TCPPortsTarget -Pn $target -oN $mainDirectory/$directory/tcp-ports-services.txt &> /dev/null &
-        echo ''; spinner "[Nmap - Identification of services and versions of TCP ports]"
-        echo ''; cat $mainDirectory/$directory/tcp-ports-services.txt | grep -v '#' | grep 'PORT\|[0-9]/tcp'; echo ''
+        echo ''; spinner "[Nmap - Identification of services and versions of TCP ports]" 1
+        echo ''; cat $mainDirectory/$directory/tcp-ports-services.txt | grep -v '#' | grep 'PORT\|[0-9]/tcp' | sed "s/.*/$indentation2&/g"
     fi    
 }
 
@@ -233,10 +257,11 @@ function serviceNameByPort(){
         21)             serviceName='ftp'   ;;
         22)             serviceName='ssh'   ;;
         25 | 465 | 587) serviceName='smtp'  ;;
-        80)             serviceName='http'  ;;
+        80 | 8080)      serviceName='http'  ;;
         443)            serviceName='https' ;;
         139 | 445)      serviceName='smb'   ;;
         161 | 162)      serviceName='snmp'  ;;
+        3389)           serviceName='rdp'   ;;
     esac
 
     echo "$serviceName"
@@ -247,12 +272,13 @@ function portConfByServiceName(){
 
     case $1 in
         FTP)   TCPPortsTarget='21'      ;;
-        SSH)   TCPPortsTarget='22'      ;;
+        SSH)   TCPPortsTarget='22'      ;;        
+        SMTP)  TCPPortsTarget='25'      ;;        
         HTTP)  TCPPortsTarget='80'      ;;
-        HTTPS) TCPPortsTarget='443'     ;;        
-        SMTP)  TCPPortsTarget='25'      ;;
-        SMB)   TCPPortsTarget='445'     ;;
+        HTTPS) TCPPortsTarget='443'     ;;
+        SMB)   TCPPortsTarget='445'     ;;        
         SNMP)  UDPPortsTarget='161,162' ;;
+        RDP)   TCPPortsTarget='3389'    ;;        
     esac
 }
 
@@ -264,33 +290,41 @@ function basicServiceEnumTCP(){
 
     case $1 in
         21) # FTP
-
+            
         ;;    
         22) # SSH
-
+            
         ;;
-        80) # HTTP
+        80 | 8080) # HTTP
+            echo -e "${GREEN}$indentation1[$1/TCP (${serviceName^^})]${NC}"
+
             whatweb -v -a 1 http://$target:$1/ | tee $mainDirectory/$serviceName/whapweb-tcp-$1.txt &> /dev/null &
-            spinner "[WhatWeb]"
+            spinner "[WhatWeb]" 2
             dirsearch -u http://$target:$1/ -o $(pwd)/$mainDirectory/$serviceName/dirsearch-tcp-$1.txt &> /dev/null &
-            spinner "[dirsearch]"
+            spinner "[dirsearch]" 2
         ;;
         443) # HTTPS
+            echo -e "${GREEN}$indentation1[$1/TCP (${serviceName^^})]${NC}"
+
             whatweb -v -a 1 https://$target:$1/ | tee $mainDirectory/$serviceName/whapweb-tcp-$1.txt &> /dev/null &
-            spinner "[WhatWeb]"
+            spinner "[WhatWeb]" 2
             dirsearch -u https://$target:$1/ -o $(pwd)/$mainDirectory/$serviceName/dirsearch-tcp-$1.txt &> /dev/null &
-            spinner "[dirsearch]"
+            spinner "[dirsearch]" 2
         ;;            
         25 | 465 | 587) # SMTP/S
+            echo -e "${GREEN}$indentation1[$1/TCP (${serviceName^^})]${NC}"
+
             smtp-user-enum -M VRFY -U /usr/share/seclists/Usernames/top-usernames-shortlist.txt -t $target -p $1 | tee $mainDirectory/$serviceName/smtp-user-enum-vrfy-top-tcp-$1.txt &> /dev/null &
-            spinner "[smtp-user-enum - VRFY]"
+            spinner "[smtp-user-enum - VRFY]" 2
         ;;
         139 | 445) # NetBIOS y SMB
-            smbclient -N -L $target --option='client min protocol=NT1' | tee $mainDirectory/$serviceName/smbclient-tcp-$1.txt &> /dev/null &
-            spinner "[smbclient]"
-            smbmap -H $target | tee $mainDirectory/$serviceName/smbmap-tcp-$1.txt &> /dev/null &
-            spinner "[SMBMap]"
-        ;;                                    
+            echo -e "${GREEN}$indentation1[139/TCP (NetBIOS), 445/TCP (SMB)]${NC}"
+
+            smbclient -N -L $target --option='client min protocol=NT1' 2>/dev/null | tee $mainDirectory/$serviceName/smbclient.txt &> /dev/null &
+            spinner "[smbclient]" 2
+            smbmap -H $target | tee $mainDirectory/$serviceName/smbmap.txt &> /dev/null &
+            spinner "[SMBMap]" 2
+        ;;
     esac
 }
 
@@ -307,23 +341,23 @@ function fullServiceEnumTCP(){
         22) # SSH
 
         ;;
-        80) # HTTP
+        80) # HTTP        
             dirsearch -u http://$target:$1/ -o $(pwd)/$mainDirectory/$serviceName/dirsearch-extension-tcp-$1.txt -e php,aspx,jsp,html,js,txt,bak -f &> /dev/null &
-            spinner "[dirsearch - Extension (php,aspx,jsp,html,js,txt,bak)]"
+            spinner "[dirsearch - Extension (php,aspx,jsp,html,js,txt,bak)]" 2
         ;;            
         443) # HTTPS
             dirsearch -u https://$target:$1/ -o $(pwd)/$mainDirectory/$serviceName/dirsearch-extension-tcp-$1.txt -e php,aspx,jsp,html,js,txt,bak -f &> /dev/null &
-            spinner "[dirsearch - Extension (php,aspx,jsp,html,js,txt,bak)]"            
+            spinner "[dirsearch - Extension (php,aspx,jsp,html,js,txt,bak)]" 2
         ;;
         25 | 465 | 587) # SMTP/S
             smtp-user-enum -M EXPN -U /usr/share/seclists/Usernames/top-usernames-shortlist.txt -t $target -p $1 | tee $mainDirectory/$serviceName/smtp-user-enum-expn-top-tcp-$1.txt &> /dev/null &
-            spinner "[smtp-user-enum - EXPN]"
+            spinner "[smtp-user-enum - EXPN]" 2
             smtp-user-enum -M RCPT -U /usr/share/seclists/Usernames/top-usernames-shortlist.txt -t $target -p $1 | tee $mainDirectory/$serviceName/smtp-user-enum-rcpt-top-tcp-$1.txt &> /dev/null &
-            spinner "[smtp-user-enum - RCPT]"
+            spinner "[smtp-user-enum - RCPT]" 2
         ;;
         139 | 445) # NetBIOS y SMB
-            smbmap -R -H $target | tee $mainDirectory/$serviceName/smbmap-recursive-tcp-$1.txt &> /dev/null &
-            spinner "[SMBMap - Recursive]"
+            smbmap -R -H $target | tee $mainDirectory/$serviceName/smbmap-recursive.txt &> /dev/null &
+            spinner "[SMBMap - Recursive]" 2
         ;;                                    
     esac
 }
@@ -335,19 +369,29 @@ function vulnServiceEnumTCP(){
 
     case $1 in
         21) # FTP
-            nmap -p $1 --script=vuln $target -oN $mainDirectory/$vulnDirectory/nmap-$serviceName-tcp-$1.txt
+            echo -e "${GREEN}$indentation1[$1/TCP (${serviceName^^})]${NC}"            
+            nmap -p $1 --script=vuln $target -oN $mainDirectory/$vulnDirectory/nmap-$serviceName-tcp-$1.txt &> /dev/null &
+            spinner "[Nmap]" 2
         ;;    
         22) # SSH
-            nmap -p $1 --script=vuln $target -oN $mainDirectory/$vulnDirectory/nmap-$serviceName-tcp-$1.txt
+            echo -e "${GREEN}$indentation1[$1/TCP (${serviceName^^})]${NC}"
+            nmap -p $1 --script=vuln $target -oN $mainDirectory/$vulnDirectory/nmap-$serviceName-tcp-$1.txt &> /dev/null &
+            spinner "[Nmap]" 2
         ;;
-        80 | 443) # HTTP/S
-            nmap -p $1 --script=vuln $target -oN $mainDirectory/$vulnDirectory/nmap-$serviceName-tcp-$1.txt
+        80 | 8080 | 443) # HTTP/S
+            echo -e "${GREEN}$indentation1[$1/TCP (${serviceName^^})]${NC}"
+            nmap -p $1 --script=vuln $target -oN $mainDirectory/$vulnDirectory/nmap-$serviceName-tcp-$1.txt &> /dev/null &
+            spinner "[Nmap]" 2
         ;;
         25 | 465 | 587) # SMTP/S
-            nmap -p $1 --script=vuln $target -oN $mainDirectory/$vulnDirectory/nmap-$serviceName-tcp-$1.txt
+            echo -e "${GREEN}$indentation1[$1/TCP (${serviceName^^})]${NC}"
+            nmap -p $1 --script=vuln $target -oN $mainDirectory/$vulnDirectory/nmap-$serviceName-tcp-$1.txt &> /dev/null &
+            spinner "[Nmap]" 2
         ;;
         139 | 445) # NetBIOS y SMB
-            
+            echo -e "${GREEN}$indentation1[139/TCP (NetBIOS), 445/TCP (SMB)]${NC}"
+            nmap -p 139,445 --script=vuln $target -oN $mainDirectory/$vulnDirectory/nmap-$serviceName.txt &> /dev/null &
+            spinner "[Nmap]" 2
         ;;
     esac
 }
@@ -356,40 +400,16 @@ function vulnServiceEnumTCP(){
 function basicServiceEnumUDP(){
     # obtención de nombre de servicio según puerto UDP
     serviceName="$(serviceNameByPort $1)"
-    directoryCreation $serviceName
+    directoryCreation $serviceName    
 
     case $1 in        
         161 | 162) # SNMP
+            echo -e "${GREEN}$indentation1[161/UDP, 162/UDP (${serviceName^^})]${NC}"
+
             sudo nmap -sC -sV -sU -p 161,162 -Pn $target -oN $mainDirectory/$serviceName/nmap-snmp-udp-161-162.txt &> /dev/null &
-            spinner "[Nmap]"
+            spinner "[Nmap]" 2
             snmp-check -p 161 $target | tee $mainDirectory/$serviceName/snmp-check-udp-161.txt &> /dev/null &
-            spinner "[snmp-check]"
-        ;;                                    
-    esac
-}
-
-# función de enumeración full de puertos UDP
-function fullServiceEnumUDP(){
-    # obtención de nombre de servicio según puerto UDP
-    serviceName="$(serviceNameByPort $1)"
-    directoryCreation $serviceName
-
-    case $1 in        
-        161 | 162) # SNMP
-
-        ;;                                    
-    esac
-}
-
-# función de enumeración de vulnerabilidades de puertos UDP
-function vulnServiceEnumUDP(){
-    # obtención de nombre de servicio según puerto UDP
-    serviceName="$(serviceNameByPort $1)"
-    directoryCreation $serviceName
-
-    case $1 in        
-        161 | 162) # SNMP
-
+            spinner "[snmp-check]" 2
         ;;                                    
     esac
 }
@@ -430,40 +450,49 @@ main() {
     IFS=', ' read -r -a udpPorts <<< "$UDPPortsTarget"
 
     case $mode in        
-        basic)
+        basic)        
             # enumeración básica de puertos TCP
-            for port in "${tcpPorts[@]}"; do
-                basicServiceEnumTCP $port
-            done
+            if [ -n "$tcpPorts" ]; then
+                echo ''; stageProcessTitle "basic enumeration (tcp ports)" 1; echo ''
+                for port in "${tcpPorts[@]}"; do
+                    basicServiceEnumTCP $port
+                done
+            fi
 
             # enumeración básica de puertos UDP
-            for port in "${udpPorts[@]}"; do
-                basicServiceEnumUDP $port
-            done
+            if [ -n "$udpPorts" ]; then
+                echo ''; stageProcessTitle "basic enumeration (udp ports)" 1; echo ''
+                for port in "${udpPorts[@]}"; do
+                    basicServiceEnumUDP $port
+                done
+            fi
         ;;
-        full)
+        full)            
             # enumeración full de puertos TCP
-            for port in "${tcpPorts[@]}"; do
-                basicServiceEnumTCP $port
-                fullServiceEnumTCP $port
-            done
-
-            # enumeración full de puertos UDP
-            for port in "${udpPorts[@]}"; do
-                basicServiceEnumUDP $port
-                fullServiceEnumUDP $port                
-            done
+            if [ -n "$tcpPorts" ]; then
+                echo ''; stageProcessTitle "full enumeration (tcp ports)" 1; echo ''
+                for port in "${tcpPorts[@]}"; do
+                    basicServiceEnumTCP $port
+                    fullServiceEnumTCP $port
+                done            
+            fi
+            
+            # enumeración de vulnerabilidades de puertos TCP
+            if [ -n "$tcpPorts" ]; then
+                echo ''; stageProcessTitle "vulnerability enumeration" 1; echo ''
+                for port in "${tcpPorts[@]}"; do
+                    vulnServiceEnumTCP $port
+                done
+            fi
         ;;
-        vuln)
-            # vulnerabilidades de puertos TCP
-            for port in "${tcpPorts[@]}"; do
-                vulnServiceEnumTCP $port
-            done
-
-            # vulnerabilidades de puertos UDP
-            for port in "${udpPorts[@]}"; do
-                vulnServiceEnumUDP $port
-            done            
+        vuln)            
+            # enumeración de vulnerabilidades de puertos TCP
+            if [ -n "$tcpPorts" ]; then
+                echo ''; stageProcessTitle "vulnerability enumeration" 1; echo ''
+                for port in "${tcpPorts[@]}"; do
+                    vulnServiceEnumTCP $port
+                done
+            fi
         ;;
     esac
 
